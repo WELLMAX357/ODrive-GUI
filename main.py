3bf653ba54f8438f68079b5423535df1ee9e7562
Masafumi <asai020403@gmail.com>
2023-01-26 15:28:13 +0900

    :sparkles: Add TORO configuration

--- main.py
+++ main.py
@@ -8,17 +8,16 @@ import config
 
 
 def calibrate():
-    odrv0.axis0.requested_state = odrv0.axis1.requested_state = AxisState.MOTOR_CALIBRATION
+    odrv0.axis0.requested_state = AxisState.MOTOR_CALIBRATION
 
 def loop():
-    odrv0.axis0.requested_state = odrv0.axis1.requested_state = AxisState.CLOSED_LOOP_CONTROL
+    odrv0.axis0.requested_state = AxisState.CLOSED_LOOP_CONTROL
 
 def stop():
-    odrv0.axis0.requested_state = odrv0.axis1.requested_state = AxisState.IDLE
+    odrv0.axis0.requested_state = AxisState.IDLE
 
 def sendVelocity():
     odrv0.axis0.controller.input_vel = int(values[0])
-    odrv0.axis1.controller.input_vel = int(values[0])
 
 
 print('Searching ODrive...')
@@ -46,13 +45,6 @@ layout = [
     sg.InputText(default_text='0', size=(5, 1), key='txt0'),
     sg.Button('Set', key='btn0')],
     
-    # axis1
-    [sg.Text('Axis1'),
-    sg.Slider(range=(-300,300), default_value=0, resolution=10, orientation='h',
-    enable_events=True, disable_number_display=True, size=(50,40), key='sl1'),
-    sg.InputText(default_text='0', size=(5, 1), key='txt1'),
-    sg.Button('Set', key='btn1')],
-    
     # button
     [sg.Button('Calibration'), sg.Button('STOP'), sg.Button('LOOP'), sg.Button('Send Velocity')],
 ]
@@ -73,17 +65,11 @@ while True:
     elif event == sg.TIMEOUT_EVENT:
         window['voltage'].Update(round(float(odrv0.vbus_voltage), 2))
 
-    # update axis0 value
+    # update axis1 value
     elif event == 'sl0':
         window['txt0'].Update(int(values['sl0']))
     elif event == 'btn0':
         window['sl0'].Update(int(values['txt0']))
-
-    # update axis1 value
-    elif event == 'sl1':
-        window['txt1'].Update(int(values['sl1']))
-    elif event == 'btn1':
-        window['sl1'].Update(int(values['txt1']))
 
     # start calibration
     elif event == 'Calibration':
@@ -100,6 +86,5 @@ while True:
     # send velocity
     elif event == 'Send Velocity':
         odrv0.axis0.controller.input_vel = int(values['txt0'])
-        odrv0.axis1.controller.input_vel = int(values['txt1'])
 
 window.close()
--- test.py
+++ test.py
@@ -8,11 +8,11 @@ odrv0.axis0.controller.config.vel_integrator_gain = 0.05
 odrv0.axis0.controller.config.control_mode = 2
 # 5.51328895422 / (<pole pairs> * <KV>)
 # TORO
-# odrv0.axis0.motor.config.pole_pairs = 3
-# odrv0.axis0.sensorless_estimator.config.pm_flux_linkage = 5.51328895422 / (3 * 2100)
+odrv0.axis0.motor.config.pole_pairs = 3
+odrv0.axis0.sensorless_estimator.config.pm_flux_linkage = 5.51328895422 / (3 * 2100)
 # TMotor
-odrv0.axis0.sensorless_estimator.config.pm_flux_linkage = 5.51328895422 / (11 * 420)
-odrv0.axis0.motor.config.pole_pairs = 11
+# odrv0.axis0.sensorless_estimator.config.pm_flux_linkage = 5.51328895422 / (11 * 420)
+# odrv0.axis0.motor.config.pole_pairs = 11
 odrv0.axis0.motor.config.requested_current_range = 200
 odrv0.axis0.motor.config.current_lim_margin = 30
 odrv0.axis0.motor.config.resistance_calib_max_voltage = 3.5
@@ -25,11 +25,11 @@ odrv0.axis1.controller.config.vel_gain = 0.01
 odrv0.axis1.controller.config.vel_integrator_gain = 0.05
 odrv0.axis1.controller.config.control_mode = 2
 # TORO
-# odrv0.axis1.motor.config.pole_pairs = 3
-# odrv0.axis1.sensorless_estimator.config.pm_flux_linkage = 5.51328895422 / (3 * 2100)
+odrv0.axis1.motor.config.pole_pairs = 3
+odrv0.axis1.sensorless_estimator.config.pm_flux_linkage = 5.51328895422 / (3 * 2100)
 # TMotor
-odrv0.axis1.sensorless_estimator.config.pm_flux_linkage = 5.51328895422 / (11 * 420)
-odrv0.axis1.motor.config.pole_pairs = 11
+# odrv0.axis1.sensorless_estimator.config.pm_flux_linkage = 5.51328895422 / (11 * 420)
+# odrv0.axis1.motor.config.pole_pairs = 11
 odrv0.axis1.motor.config.requested_current_range = 200
 odrv0.axis1.motor.config.current_lim_margin = 30
 odrv0.axis1.motor.config.resistance_calib_max_voltage = 3.5
